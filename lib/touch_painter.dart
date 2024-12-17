a82b8d91347490b43bceb9cd61f4a0c4084181cd
KimHyungSik <colrog968@gmail.com>
2024-12-18 08:22:18 +0900

    touch painter stroke

--- lib/touch_painter.dart
+++ lib/touch_painter.dart
@@ -14,13 +14,21 @@ class TouchPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final paint = Paint()
+    final circlePaint = Paint()
       ..style = PaintingStyle.fill
       ..color = Colors.white;
 
+    final strokePaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2
+      ..color = Colors.white;
+
     for (var entry in touches.entries) {
-      paint.color = touchColors[entry.key % touchColors.length];
-      canvas.drawCircle(entry.value, 30, paint); // Draw a circle at each touch
+      circlePaint.color = touchColors[entry.key % touchColors.length];
+      strokePaint.color = touchColors[entry.key % touchColors.length];
+
+      canvas.drawCircle(entry.value, 50, circlePaint); // Draw a circle at each touch
+      canvas.drawCircle(entry.value, 60, strokePaint); // Draw a stroke at each touch
     }
   }
 
